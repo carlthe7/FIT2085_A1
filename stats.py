6a3941ec302053f109c1a0d7f0b8616dd818facb
nhuy0018 <nhuy0018@student.monash.edu>
2023-08-20 23:43:49 +1000

    Add comments and finalise the stats.py

--- stats.py
+++ stats.py
@@ -1,6 +1,7 @@
 import abc
 
 from data_structures.referential_array import ArrayR
+from data_structures.stack_adt import ArrayStack
 
 class Stats(abc.ABC):
 
@@ -28,15 +29,14 @@ class SimpleStats(Stats):
         self.defense = defense
         self.speed = speed
         self.max_hp = max_hp 
-        pass
 
-    def get_attack(self): #return value 
+    def get_attack(self): #return attack value  O(1) 
         return self.attack
-    def get_defense(self):
+    def get_defense(self): #return defense value  O(1)
         return self.defense
-    def get_speed(self):
-        return self.speed
-    def get_max_hp(self):
+    def get_speed(self): #return speed value  O(1)
+        return self.speed 
+    def get_max_hp(self): #return max_hp value  O(1)
         return self.max_hp
     
 
@@ -55,16 +55,78 @@ class ComplexStats(Stats):
         self.speed_formula = speed_formula
         self.max_hp_formula = max_hp_formula
 
-    def get_attack(self, level: int):
-        raise NotImplementedError
+    def get_attack(self, level: int): #return attack value   O(1)
+        return self.calculate_formula(self.attack_formula,level)    
+    def get_defense(self, level: int): #return defense value  O(1)
+        return self.calculate_formula(self.defense_formula,level)    
+    def get_speed(self, level: int): #return speed value  O(1)
+        return self.calculate_formula(self.speed_formula,level)    
+    def get_max_hp(self, level: int): #return max_hp value O(1)
+       return self.calculate_formula(self.max_hp_formula,level)     
+    
+    def calculate_formula(self, formula:ArrayR[str], level_num: int) -> int:
+        stored_number = ArrayStack(len(formula))
 
-    def get_defense(self, level: int):
-        raise NotImplementedError
+        for element in formula: # O(n) Loop through the elememnt
+            
+            if element == "level":
+                stored_number.push(level_num)  #push the level to the stack O(1)
+     
+            elif element == "sqrt":
+                result = self.square_roorts(stored_number.pop()) #calculate the square roots of the number then push to the stack O(1)
+                stored_number.push(result)
 
-    def get_speed(self, level: int):
-        raise NotImplementedError
+            elif element == "middle":
+                result_array = self.middle(stored_number) #calculate the middle then push to the stack O(1)
+                stored_number.push(result_array)
+            
+            elif element == "+": #calculate the total then push to the stack O(1)
+                num2 = stored_number.pop()
+                num1 = stored_number.pop()
+                result = num2 + num1
+                stored_number.push(result) 
 
-    def get_max_hp(self, level: int):
-        raise NotImplementedError
+            elif element == "-": #calculate the difference then push to the stack O(1)
+                num2 = stored_number.pop()
+                num1 = stored_number.pop()
+                result =  num1 - num2
+                stored_number.push(result) 
 
-    #def calc_forumla(self,formula: ArrayR[str],level: int) -> int: 
+            elif element == "/": #calculate the division then push to the stack O(1)
+                num2 = stored_number.pop()
+                num1 = stored_number.pop()
+                result =  num1/num2
+                stored_number.push(result)
+
+            elif element == "*": #calculate the multiplication then push to the stack O(1)
+                num2 = stored_number.pop()
+                num1 = stored_number.pop()
+                result =  num1*num2
+                stored_number.push(result) 
+
+            elif element == "power": #calculate the power number then push to the stack O(1)
+                num2 = stored_number.pop()
+                num1 = stored_number.pop()
+                result =  num1**num2
+                stored_number.push(result)
+            else: #push and convert to int to the stack O(1)
+                stored_number.push(int(element))
+
+        return stored_number.pop() #return the final number in the stack O(1)
+        
+
+    def square_roorts(self,num:int) -> int: #calculate the square roots O(1)
+        return num**0.5
+
+
+    def middle(self,list: ArrayStack) -> int: #find the median of the 3 numbers O(1)
+        num1 = list.pop()
+        num2 = list.pop()
+        num3 = list.pop()
+        if (num1 < num2 and num1 > num3) or (num1 < num3 and num1 > num2):
+            median = num1
+        elif (num2 < num3 and num2 > num1) or (num2 < num1 and num2 > num3):
+            median = num2
+        else: 
+            median = num3
+        return median
